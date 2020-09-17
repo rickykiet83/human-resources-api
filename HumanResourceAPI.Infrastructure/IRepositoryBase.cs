782b543fa31656001ab74da66b85bfd3ae029ab6
Kiệt Phạm Quang Anh <phamquanganhkiet@gmail.com>
2020-09-17 22:28:47 +1000

    Tạo cấu trúc thư mục dự án

---
+++ HumanResourceAPI.Infrastructure/IRepositoryBase.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace HumanResourceAPI.Infrastructure
+{
+    public interface IRepositoryBase<T, K> where T : class
+    {
+        T FindById(K id, params Expression<Func<T, object>>[] includeProperties);
+        
+        Task<T> FindByIdAsync(K id, params Expression<Func<T, object>>[] includeProperties);
+
+        T FindSingle(Expression<Func<T, bool>> predicate, params Expression<Func<T, object>>[] includeProperties);
+
+        Task<T> FindSingleAsync(Expression<Func<T, bool>> predicate,
+            params Expression<Func<T, object>>[] includeProperties);
+
+        IQueryable<T> FindAll(params Expression<Func<T, object>>[] includeProperties);
+
+        IQueryable<T> FindAll(Expression<Func<T, bool>> predicate, params Expression<Func<T, object>>[] includeProperties);
+
+    }
+}
