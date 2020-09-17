d0cc256c54d71f7677401d18d93a71228087f95d
Kiệt Phạm Quang Anh <phamquanganhkiet@gmail.com>
2020-09-17 22:49:36 +1000

    Configure CORS, IIS

--- HumanResourceAPI/Startup.cs
+++ HumanResourceAPI/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using HumanResourceAPI.Extensions;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -27,6 +28,9 @@ namespace HumanResourceAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.ConfigureCors();
+            services.ConfigureIISIntegration();
+            
             services.AddControllers();
         }
 
